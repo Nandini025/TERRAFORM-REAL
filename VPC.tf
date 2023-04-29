bcc87cdb961ac1b2a75e1d64e16f4311565b31a2
Nandini025 <karlapudinandini025@gmail.com>
2023-04-29 18:08:55 +0530

    Changed VPC to main name

--- VPC.tf
+++ VPC.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "VPC" { # the name which we mentioned here is only understood by terraform
+resource "aws_vpc" "main" { # the name which we mentioned here is only understood by terraform
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -7,7 +7,7 @@ resource "aws_vpc" "VPC" { # the name which we mentioned here is only understood
   }
 }
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.VPC.id  # it will fetch VPC ID from above code
+  vpc_id     = aws_vpc.main.id  # it will fetch VPC ID from above code
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -16,7 +16,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.VPC.id  # it will fetch VPC ID from above code
+  vpc_id     = aws_vpc.main.id  # it will fetch VPC ID from above code
   cidr_block = "10.0.2.0/24"
 
   tags = {
@@ -25,7 +25,7 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_internet_gateway" "IGW" {
-  vpc_id = aws_vpc.VPC.id
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "Auto-IGW"
