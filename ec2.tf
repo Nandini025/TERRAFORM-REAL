c459de0bd32973186842c138762bec2ff6d365fb
Nandini025 <karlapudinandini025@gmail.com>
2023-05-05 18:42:29 +0530

    Modified

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  key_name = aws_key_pair.key.key_name
   tags = {
     Name = "WEB"
   }
@@ -17,4 +17,8 @@ data "aws_region" "current" {}
 
 data "aws_vpc" "existing-vpc-info" {
   id = "VPC-07204ah520q" # give vpc id which is existed one
+}
+resource "aws_key_pair" "key" {
+  key_name   = "key"
+  public_key = file("${path.module}/terraform.pub") #this will read file and fetch the content
 }
